f5b6db4b89996ab4423c85d83dc637539790df57
Bennett <bennettjohnson0@gmail.com>
2022-09-26 15:23:38 -0500

    added font, setup random number gen, begin adding instructions

---
+++ REQS.md
@@ -0,0 +1,14 @@
+### Chip-8 Emulator Requirements
+---
+A chip-8 contains the following components:
+* 16 8-bit registers
+* 4k bytes of memory
+* 16-bit index register
+* 16-bit program counter
+* 16-level stack
+* 8-bit stack pointer
+* 8-bit delay timer
+* 8-bit sound timer
+* 16 input keys
+* 64x32 monochrome display memory
+* 
--- source/Chip8.cpp
+++ source/Chip8.cpp
@@ -1,5 +1,7 @@
 #include <cstdint>
 #include <fstream>
+#include <chrono>
+#include <random>
 
 class Chip8 {
 
@@ -16,8 +18,58 @@ public:
     uint32_t video[64 * 32]{};
     uint16_t opcode;
 
-    const unsigned int START_ADDRESS = 0x200;
+    std::default_random_engine randGen;
+    std::uniform_int_distribution<uint8_t> randByte;
 
+    const unsigned int START_ADDRESS = 0x200;
+    const unsigned int FONTSET_START_ADDRESS = 0x50;
+    static const unsigned int FONTSET_SIZE = 80;
+    
+    uint8_t fontset[FONTSET_SIZE] = 
+    {
+        // font characters represented as 16x5 sprites
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        /*  example:
+            11110000
+            10010000
+            10010000
+            10010000
+            11110000
+        */
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    };
+
+
+
+    Chip8::Chip8() 
+    //seed random using system clock
+    : randGen(std::chrono::system_clock::now().time_since_epoch().count()) 
+    {
+        // pc begins outside of reserved memory
+        pc = START_ADDRESS;
+
+        // load font into memory
+        for(unsigned int i = 0; i < FONTSET_SIZE; ++i) {
+            memory[FONTSET_START_ADDRESS + i] = fontset[i];
+        }
+
+        // initialize random
+        randByte = std::uniform_int_distribution<uint8_t>(0,255);
+    }
 
     void Chip8::LoadROM(char const* filename) {
 
@@ -45,4 +97,13 @@ public:
         }
 
     }
+
+    // INSTRUCTIONS:
+    void Chip8::OP_00E0() {
+        // cls | clear screen
+        memset(video, 0, sizeof(video));
+    }
+
+
+
 };
